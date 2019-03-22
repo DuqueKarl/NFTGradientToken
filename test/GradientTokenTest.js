d2009f0f952729fa9965bdfa4d2fbf59a02f0a22
LAPTOP-9A4TGPS4\ppin <duquekarl@gmail.com>
2019-03-22 02:08:35 +0100

    Update GradientTokenTest.js
    edit formatting

--- test/GradientTokenTest.js
+++ test/GradientTokenTest.js
@@ -8,19 +8,19 @@ contract("Gradient token", accounts => {
     assert.equal(owner, accounts[0]);
   });
 
-	describe("mint", () => {
-	  it("creates token with specified outer and inner colors", async () => {
-			let instance = await GradientToken.deployed();
-			let owner = await instance.owner();
+  describe("mint", () => {
+    it("creates token with specified outer and inner colors", async () => {
+      let instance = await GradientToken.deployed();
+      let owner = await instance.owner();
 
-			let token0 = await instance.mint("#ff00dd", "#ddddff");
-			let token1 = await instance.mint("#111111", "#ffff22");
-			let token2 = await instance.mint("#111111", "#33ff33");
+      let token0 = await instance.mint("#ff00dd", "#ddddff");
+      let token1 = await instance.mint("#111111", "#ffff22");
+      let token2 = await instance.mint("#111111", "#33ff33");
 			
-			let gradients1 = await instance.getGradient( 1 );
-			assert.equal(gradients1.outer, "#111111", "Outer should be #111111");
-			assert.equal(gradients1.inner, "#ffff22", "Inner should be #ffff22");
-	  });
+      let gradients1 = await instance.getGradient( 1 );
+      assert.equal(gradients1.outer, "#111111", "Outer should be #111111");
+      assert.equal(gradients1.inner, "#ffff22", "Inner should be #ffff22");
+    });
 		
   });
   
